7b34fd29f93ea52f50f7f26a65bc48d4981708d6
v1endr3 <xiayuanming4m@gmail.com>
2024-01-31 22:18:01 +0800

    add ApplicationMain::main

--- demo-starter/src/main/java/org/example/ApplicationMain.java
+++ demo-starter/src/main/java/org/example/ApplicationMain.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author V1endr3
  */
+@SpringBootApplication
 public class ApplicationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
+    }
 }
