fa1d3c6bc53853877e6cb23c15d94382cbc01a03
v1endr3 <xiayuanming4m@gmail.com>
2024-09-04 13:46:45 +0800

    push camel

--- Camel/src/main/java/org/example/CamelSpringMain.java
+++ Camel/src/main/java/org/example/CamelSpringMain.java
@@ -1,12 +1,12 @@
 package org.example;
 
 import org.apache.camel.spring.Main;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class CamelSpringMain {
     public static void main(String[] args) throws Exception {
         Main main = new Main();
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/camel-kafka.xml");
+        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("file:/local/camel-run.xml");
         main.setApplicationContext(context);
         main.run();
     }
