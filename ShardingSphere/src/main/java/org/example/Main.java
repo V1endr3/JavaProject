b01a6d465f71f4c80571e77a2bf4b9f18981f665
v1endr3 <xiayuanming4m@gmail.com>
2024-10-25 15:41:29 +0800

    camel test

--- ShardingSphere/src/main/java/org/example/Main.java
+++ ShardingSphere/src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import org.apache.shardingsphere.infra.config.mode.ModeConfiguration;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
