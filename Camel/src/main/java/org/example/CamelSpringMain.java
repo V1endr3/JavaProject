f914849401f6858f6934c54dfed92ede9890f349
v1endr3 <xiayuanming4m@gmail.com>
2024-10-31 10:35:12 +0800

    camel

--- Camel/src/main/java/org/example/CamelSpringMain.java
+++ Camel/src/main/java/org/example/CamelSpringMain.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class CamelSpringMain {
     public static void main(String[] args) throws Exception {
         Main main = new Main();
-        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("file:/Camel/conf/rest-basic.xml");
+        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("file:./Camel/conf/rest-basic.xml");
         main.setApplicationContext(context);
         main.run();
     }
