ba74124b616c5ddd3d764c34c8fb436fc7410427
v1endr3 <xiayuanming4m@gmail.com>
2024-11-01 09:25:20 +0800

    camel

--- Camel/src/main/java/org/example/CamelSpringMain.java
+++ Camel/src/main/java/org/example/CamelSpringMain.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class CamelSpringMain {
     public static void main(String[] args) throws Exception {
         Main main = new Main();
-        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("file:./Camel/conf/logic/loop.xml");
+        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("file:./Camel/conf/logic/filter.xml");
         main.setApplicationContext(context);
         main.run();
     }
